f83af6b4c2d97bdd09d50972f9099b85a2668cac
Jose Mosquera <jojumofu@gmail.com>
2023-08-03 14:13:15 -0500

    feat: create delivery struct

--- main.go
+++ main.go
@@ -1,8 +1,25 @@
 package main
 
 import (
+	"time"
+
 	"github.com/gin-gonic/gin"
 )
+
+type Delivery struct {
+	ID           string    `json:"id"`
+	CreationDate time.Time `json:"creation_date"`
+	State        string    `json:"state"`
+	Pickup       struct {
+		PickupLat float64 `json:"pickup_lat"`
+		PickupLon float64 `json:"pickup_lon"`
+	} `json:"pickup"`
+	Dropoff struct {
+		DropoffLat float64 `json:"dropoff_lat"`
+		DropoffLon float64 `json:"dropoff_lon"`
+	} `json:"dropoff"`
+	ZoneID string `json:"zone_id"`
+}
 
 func main() {
 	r := gin.Default()
